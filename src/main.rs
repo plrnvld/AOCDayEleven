4b3cca4fc0328f0f09a28c4bd6183fc228154785
plrnvld <paulrijneveld@gmail.com>
2021-12-11 23:54:40 +0000

    Runs with 'index out of bounds' errors

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,16 @@ fn main() {
     let grid = grid_base.as_mut_slice();
     fill_grid(grid);
 
-    println!("Hello!");
-    println!("{}", grid[0][0]);
-    println!("{}", grid[1][0]);
-    println!("{}", grid[2][0]);
-    println!();
+    let mut total_flashes = 0;
+    for _ in 0..100 {
+        total_flashes += execute_round(grid);
+    }
 
-    println!("{}", grid[9][6]);
-    println!("{}", grid[9][7]);
-    println!("{}", grid[9][8]);
-    println!("{}", grid[9][9]);
+    println!("Total flashes {}", total_flashes);
 }
 
 fn fill_grid(grid_to_fill: &mut [&mut [u32]]) -> () {
+    
     if let Ok(lines) = read_lines("Input.txt") {
         let mut line_num = 0;
         for line in lines {
@@ -41,6 +38,67 @@ fn fill_grid(grid_to_fill: &mut [&mut [u32]]) -> () {
             }
         }
     }
+}
+
+fn inc(grid: &mut [&mut [u32]], x: i32, y: i32) -> () {
+    if x >= 0 || x <= 9 || y >= 0 || y <= 9 {
+        let curr_val = grid[x][y];
+        let new_val = if curr_val < 10 { curr_val + 1 } else { curr_val };
+        grid[x as usize][y as usize] = new_val;
+    }
+}
+
+fn flash(grid: &mut [&mut [u32]], x: i32, y: i32) -> () {
+    inc(grid, x - 1, y - 1);
+    inc(grid, x, y - 1);
+    inc(grid, x + 1, y - 1);
+    inc(grid, x - 1, y);
+    inc(grid, x + 1, y);
+    inc(grid, x - 1, y + 1);
+    inc(grid, x, y + 1);
+    inc(grid, x + 1, y + 1);
+}
+
+fn reset(grid: &mut [&mut [u32]], x: i32, y: i32) -> () {
+    if x >= 0 || x <= 9 || y >= 0 || y <= 9 {
+        grid[x as usize][y as usize] = 0;
+    }
+}
+
+fn execute_round(grid: &mut [&mut [u32]]) -> i32 {
+    let mut flash_count = 0;
+    let max_iter: u32 = 10;
+    
+    // Increasing
+    for y in 0..10 {
+        for x in 0..10 {
+            inc(grid, x, y);
+        }
+    }
+
+    // Flashing
+    for y in 0..10 {
+        for x in 0..10 {
+
+            let curr_val = grid[x][y];
+            if curr_val == 10 {
+                flash(grid, x as i32, y as i32);
+            }            
+        }
+    }
+
+    // Resetting + flash counting
+    for y in 0..10 {
+        for x in 0..10 {
+            let curr_val = grid[x][y];
+            if curr_val == 10 {
+                flash_count += 1; 
+                reset(grid, x as i32, y as i32);             
+            }
+        }
+    }
+
+    return flash_count;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
